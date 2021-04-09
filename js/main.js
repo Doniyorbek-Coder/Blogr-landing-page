05b3d3c704e0e9e967a922c6f2d2a130f34f4de7
Doniyor-Coder <asqaraliyev1559099@gmail.com>
2021-04-09 21:20:53 +0500

    some bugs have been fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-let darkButton=document.querySelector(".sitenav-item");
-let body=document.querySelector(".dropdown-list")
-darkButton.onclick=function(){
-    body.classList.toggle("dropdown-list");
-    body.classList.toggle("dropdown-list-show");
-    console.log(darkButton);
-}
+// let darkButton=document.querySelector(".sitenav-item");
+// let body=document.querySelector(".dropdown-list")
+// darkButton.onclick=function(){
+//     body.classList.toggle("dropdown-list");
+//     body.classList.toggle("dropdown-list-show");
+//     console.log(darkButton);
+// }
